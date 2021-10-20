be0937eb60ea4138efa0c65aa22055f5bb088ee9
york <jason945119@gmail.com>
2021-10-20 09:28:44 +0800

    修正

--- 1-物件相關問題/1.php
+++ 1-物件相關問題/1.php
@@ -4,19 +4,16 @@ class Car
 	private static $_normal_name = '西羅摩';
 	private $_name = 'york的天竺鼠車車';
 
-	// __construct 不得為 static function
 	public function __construct()
 	{
 		//echo 'instance 才會跑到這裡'.PHP_EOL;
 	}
 
-	// static
 	public static function getNormalName() : string
 	{
 		return self::$_normal_name;
 	}
 
-	// instance
 	public function getName() : string
 	{
 		return $this->_name;
@@ -39,12 +36,12 @@ class Car
  * 問題三
  * 下列程式可以執行嗎
  */
-$obj = new Car();
+//$obj = new Car();
 //echo $obj->getNormaName();
 
 /**
  * 問題三
  * 下列程式可以執行嗎
  */
-$obj = new Car();
+//$obj = new Car();
 //echo $obj->getName();
