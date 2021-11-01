664016fb234a36f3bcb7ef7fe93474ae842e92f4
york <jason945119@gmail.com>
2021-11-01 16:45:01 +0800

    2-1

---
+++ 2-php基本操作/1.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * 問題一
+ * 下列程式可以執行嗎
+ */
+//function getAll($value)
+//{
+//    $types = [
+//        1 << 0 => 'quick',
+//        1 << 1 => 'good',
+//        1 << 2 => 'cheap',
+//    ];
+//
+//    if($value <=0 ) return [0];
+//
+//    $pad = 0;
+//
+//    while ($value)
+//    {
+//        if ($value & 1) $array[] = 1 << $pad;
+//        $pad++;
+//        $value >>= 1;
+//    }
+//    $return = [];
+//    foreach($array as $type)
+//    {
+//        $return[] =  $types[$type];
+//    }
+//    return $return;
+//}
+//getAll(7 );
+
+/**
+ * 問題二
+ * 如果可以執行結果為何?
+ * 如果不能執行為什麼?
+ */
