4acb33f7344c313a2232c5c3f5a444bf5fbbafde
york <jason945119@gmail.com>
2021-11-06 22:48:13 +0800

    2-3

---
+++ 2-php基本操作/3.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * 問題
+ * 分別能印出什麼
+ */
+echo 1 <=> 1;
+echo PHP_EOL;
+echo 2 <=> 3;
+echo PHP_EOL;
+echo 3 <=> 2;
+echo PHP_EOL;
+echo "a" <=> "a";
+echo PHP_EOL;
+echo "b" <=> "c";
+echo PHP_EOL;
+echo "c" <=> "b";
